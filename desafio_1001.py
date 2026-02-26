3f4df547c3c64bd9990553f53c255ebf83856d0a
riquelmesouza01 <riquelmesouza44970@gmail.com>
2026-02-26 09:06:51 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -15,3 +15,8 @@ A = input ("Digite o 1º número")
 B = input ("Digite o 2º número")
 X = A+B
 print(f"X = {X}")
+
+A = int(input("Digite o 1º número"))
+B = int(input("Digite o 2º número"))
+X = A+B
+print(f"X = {X}")
